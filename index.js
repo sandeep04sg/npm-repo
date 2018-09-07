28816091a78a163fe5904035ee710fc24d8c2d31
sandip gupta <sandeep04gupta@gmail.com>
2018-09-07 21:52:05 +0530

    error and callback statement

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-var rect=('./rectangle');
+var rect=require('./rectangle');
 
 function solveRect(l,b) {
     console.log("solving for rectangle with l= "+l+" and b "+b);
-    if(l <=0 || b <=0){
-        console.log("rectanagle diminesion should be greater then zero : l = "+l+" and b = "+b);
-    }
-    else{
-        console.log("Area of the reactangle is : "+rect.area(l,b));
-        console.log("perimeter of the rectangle is : "+rect.perimeter(l,b));
-    }
-}
+   
+    rect(l,b,(err,rectangle) => {
+        if(err){
+            console.log("Error : ",err.message);
+        }
+        else{
+            console.log("the area of the rectangle "+l+" and b"+b+" is area "+rectangle.area());
+            console.log("the perimeter of the rectangle "+l+" and b"+b+" is area "+rectangle.perimeter());
 
+        }
+    } );
+}
+solveRect(8,50);
+solveRect(60,18);
 solveRect(2,5);
 solveRect(6,8);
